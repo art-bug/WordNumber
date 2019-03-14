c084416d87dc1ca9b15fd8f11508ca23e1771490
art-bug <artbag94@gmail.com>
2019-03-14 04:36:30 +0300

    Pre-alpha program version

--- WordNumberClient.cpp
+++ WordNumberClient.cpp
@@ -3,85 +3,44 @@
 
 #include <stdio.h>
 #include <winsock2.h>
-// Winsock2.h must be connected before windows.h
-#include <windows.h>
 #include <stdlib.h>
-#include <locale.h>
-#include "word_number.h"
+// #include "word_number.h"
+#include "oem866_word_number.h"
 
-#define PORT 5555
 #define SERVERADDRESS "127.0.0.1"
-
-#define PRESS_ANY_KEY std::cin.ignore(); std::cin.get();
-
-using ulong = unsigned long;
+#define PORT 5555
 
 void fail() {
     std::cout << "FAIL" << std::endl;
-    PRESS_ANY_KEY
+    system("pause");
 }
 
 int main() {
-    setlocale(LC_ALL, "RUS");
 
     std::cout << "NUMTOSTR DEMO CLIENT" << std::endl;
 
-	// Initialize the Winsock library
-	// Since the information returned by the function is not used,
-	// it is passed a pointer to the working buffer, converted to
-	// a pointer to the WSADATA structure. This method allows you to manage
-	// one variable, however, the buffer must be at least
-	// half a kilobyte (WSADATA structure occupies 400 bytes)
-
-    if (WSAStartup(0x0202, new WSADATA[400])) {
-        std::cout << "WSA startup error " << WSAGetLastError() << std::endl;
-        PRESS_ANY_KEY
-        return -1;
-    }
-
-	// Create a socket
-	// AF_INET - Internet socket
-	// SOCK_STREAM - stream socket (with connection setup)
-	// TCP protocol is automatically selected
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (!sock) {
-        std::cout << "Socket error " << WSAGetLastError() << std::endl;
-		WSACleanup();
-        PRESS_ANY_KEY
-        return -1;
-    }
-
-	// Specify the server address and port
-    sockaddr_in dest_addr = {};
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(PORT);
-
-	// Converting an IP address from a character format to a network
-    if (inet_addr(SERVERADDRESS) != INADDR_NONE) {
-        dest_addr.sin_addr.s_addr = inet_addr(SERVERADDRESS);
-    } else {
-		HOSTENT *pHost = gethostbyname(SERVERADDRESS);
-
-		// Obtain an IP address by the domain name of the server
-        if (pHost) {
-
-			//      Important!
-			// pHost-> h_addr_list contains an array of pointers to addresses
-            ((ulong *) &dest_addr.sin_addr)[0] = ((ulong **) pHost->h_addr_list)[0][0];
-
-        } else {
-            std::cout << "Invalid address " << SERVERADDRESS << std::endl;
-            PRESS_ANY_KEY
-            closesocket(sock);
-            WSACleanup();
+    {
+        WSADATA ws;
+        if (WSAStartup(0x0202, &ws)) {
+            std::cout << "WSA startup error " << WSAGetLastError() << std::endl;
+            system("pause");
             return -1;
         }
     }
 
+    // Specify the server address and port
+    sockaddr_in serverAddress;
+    int serverAddrSize = sizeof(serverAddress);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(SERVERADDRESS);
+    serverAddress.sin_port = htons(PORT);
+
+    SOCKET serverConnection = socket(AF_INET, SOCK_STREAM, 0);
+
     // Establish the connection
-    if (connect(sock, (sockaddr *) &dest_addr, sizeof(dest_addr))) {
+    if (connect(serverConnection, (SOCKADDR *) &serverAddress, serverAddrSize)) {
         fail();
+        std::cout << "Failed connect to server." << std::endl;
         return -1;
     }
 
@@ -90,71 +49,41 @@ int main() {
                                     return -1;  \
                                  }
 
-    const ulong MAX_SEND_BUF_SIZE = 8;
-    char sendBuffer[MAX_SEND_BUF_SIZE] = {0};
+    while (std::cin.peek() != 'q') {
+        const ushort MAX_SEND_BUF_SIZE = 8;
+        char sendBuffer[MAX_SEND_BUF_SIZE];
 
-    ulong number = 0;
+        unsigned number;
 
-    std::cin >> number;
+        std::cin >> number;
 
-    const char* strNum = wnServer.format((int)(number));
+        sprintf(sendBuffer, "%d", number);
 
-    number = htonl(number);
-
-    sprintf(sendBuffer, "%d", number);
-
-    const char* sendBufCurrentPos = sendBuffer;
-
-    while (sendBufCurrentPos - sendBuffer < MAX_SEND_BUF_SIZE) 
-    {
-        int ret = send(sock, sendBufCurrentPos, int(MAX_SEND_BUF_SIZE - (sendBufCurrentPos - sendBuffer)), 0);
+        int ret = send(serverConnection, sendBuffer, sizeof(sendBuffer), 0);
         
         if ( ret == -1 ) {
             std::cout << "client send failed" << std::endl;
-            sendBufCurrentPos = (char *) &ret;
         }
 
         IF_CONNECTION_CLOSED
 
-        sendBufCurrentPos += ret;
-    }
+        const ushort MAX_RECEIVE_BUFFER_LENGTH = 60;
+        char receiveBuffer[MAX_RECEIVE_BUFFER_LENGTH];
 
-    const ulong MAX_RECEIVE_BUFFER_LENGTH = 1024;
-    char receiveBuffer[MAX_RECEIVE_BUFFER_LENGTH];
-    char* receiveBufCurrentPos = receiveBuffer;
-
-    while ((ulong)(receiveBufCurrentPos - receiveBuffer) < MAX_RECEIVE_BUFFER_LENGTH)
-    {
-        int ret = recv(sock, (char *) receiveBufCurrentPos, 
-                             int(MAX_RECEIVE_BUFFER_LENGTH - (receiveBufCurrentPos - receiveBuffer)), 0);
+        ret = recv(serverConnection, receiveBuffer, sizeof(receiveBuffer), 0);
+        
         if (ret == -1) {
             std::cout << "client receive failed" << std::endl;
-            receiveBufCurrentPos = (char *) &ret;
         }
 
         IF_CONNECTION_CLOSED
+   
+        std::cout << receiveBuffer << std::endl;
 
-        receiveBufCurrentPos += ret;
-
-        if ((ulong)(receiveBufCurrentPos - receiveBuffer) < MAX_RECEIVE_BUFFER_LENGTH) 
-        {
-            std::cout << "[recv notice] attempt to receive " << MAX_RECEIVE_BUFFER_LENGTH << " bytes but ";
-            std::cout << (ulong)(receiveBufCurrentPos - receiveBuffer) << " received per call (";
-            std::cout << (MAX_RECEIVE_BUFFER_LENGTH - ((ulong)(receiveBufCurrentPos - receiveBuffer))) << " left to receive)";
-            std::cout << std::endl;
-        }  
+        std::cin.ignore();
     }
 
-    closesocket(sock);
-
-    if (strcmp(strNum, receiveBuffer) == 0) {    
-        std::cout << " --> " << receiveBuffer << std::endl;
-
-        PRESS_ANY_KEY
-
-    } else {
-        fail();
-    }
+    closesocket(serverConnection);
 
     WSACleanup();
 
--- WordNumberServer.cpp
+++ WordNumberServer.cpp
@@ -3,102 +3,68 @@
 
 #include <stdio.h>
 #include <winsock2.h>
-// Winsock2.h must be connected before windows.h
-#include <windows.h>
-#include <locale.h>
-#include "word_number.h"
+// #include "word_number.h"
+#include "oem866_word_number.h"
 
+#define LOCALADDRESS "127.0.0.1"
 #define PORT 5555
 
-#define PRESS_ANY_KEY std::cin.ignore(); std::cin.get();
+DWORD WINAPI serviceActiveClient(unsigned activeClientIdx);
 
-DWORD WINAPI serviceActiveClient(LPVOID clientSocket);
-
-int numActiveClients = 0;
+SOCKET connections[100];
+unsigned activeClientIdx = 0;
 
 void printNumUsers() {
-    if (numActiveClients) {
-        std::cout << numActiveClients << " user online" << std::endl;
+    if (activeClientIdx) {
+        std::cout << activeClientIdx << " user online" << std::endl;
     } else {
         std::cout << "No user online" << std::endl;
     }
 }
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-
     std::cout << "NUMTOSTR DEMO SERVER " << std::endl << std::endl;
 
-    // Initialize the Winsock library
-	// Since the information returned by the function is not used,
-	// it is passed a pointer to the working buffer, converted to
-	// a pointer to the WSADATA structure. This method allows you to manage
-	// one variable, however, the buffer must be at least
-	// half a kilobyte (WSADATA structure occupies 400 bytes)
-
-    if (WSAStartup(0x0202, new WSADATA[400])) {
-        std::cout << "WSA startup error " << WSAGetLastError() << std::endl;
-        PRESS_ANY_KEY
-        return -1;
+    {
+        WSADATA ws;
+        if (WSAStartup(0x0202, &ws)) {
+            std::cout << "WSA startup error " << WSAGetLastError() << std::endl;
+            system("pause");
+            return -1;
+        }
     }
 
-    // Create a socket
-	// AF_INET - Internet socket
-	// SOCK_STREAM - stream socket (with connection setup)
-	// TCP protocol is automatically selected
-
-    SOCKET sock = 0;
-    if (!(sock = socket(AF_INET, SOCK_STREAM, 0))) {
-        std::cout << "Socket error " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return -1;
-    }
-
-    sockaddr_in localAddress = {};
+    sockaddr_in localAddress;
+    int localAddrSize = sizeof(localAddress);
     localAddress.sin_family = AF_INET;
+    localAddress.sin_addr.s_addr = inet_addr(LOCALADDRESS);
     localAddress.sin_port = htons(PORT);
-    localAddress.sin_addr.s_addr = 0;
-    // The server accepts connections to all IP addresses
 
-    if (bind(sock, (sockaddr *) &localAddress, sizeof(localAddress))) {
-        std::cout << "Bind error " << WSAGetLastError() << std::endl;
-        closesocket(sock);
-        WSACleanup();
-        return -1;
-    }
+    SOCKET sockListen = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Waiting for connections
-    // Queue size = 5
-    if (listen(sock, 5)) {
-        std::cout << "Listen error " << WSAGetLastError() << std::endl;
-        closesocket(sock);
-        WSACleanup();
-        return -1;
-    }
+    bind(sockListen, (SOCKADDR *) &localAddress, sizeof(localAddress));
+
+    listen(sockListen, SOMAXCONN);
 
     std::cout << "Server started" << std::endl << std::endl;
     std::cout << "Waiting for connections" << std::endl << std::endl;
 
-    SOCKET clientSocket = 0;
-    sockaddr_in clientAddress = {};
+    SOCKET clientConnection;
 
-    int clientAddressSize = sizeof(clientAddress);
+    for (unsigned i = 0; i < 100; i++) {
+        clientConnection = accept(sockListen, (SOCKADDR *) &localAddress, &localAddrSize);
 
-    // Loop request queue
-    while ((clientSocket = accept(sock, (sockaddr *) &clientAddress, &clientAddressSize)))
-    {
-        // Call a new thread to serve the client
-        // For this we recommend using _beginthreadex,
-        // but, since there are no calls to the standard C functions of the library
-        // the thread does not, you can do CreateThread
-        DWORD threadID = 0;
-        CreateThread(0, 0, serviceActiveClient, &clientSocket, 0, &threadID);
+        if (!clientConnection) {
+            std::cout << "Connection error on " << activeClientIdx << " client" << std::endl << std::endl;
+        }
+        else {
+            connections[i] = clientConnection;
+            activeClientIdx++;
+            
+            CreateThread(0, 0, (LPTHREAD_START_ROUTINE) serviceActiveClient, (LPVOID) i, 0, 0);
 
-        numActiveClients++;
-
-        HOSTENT *pHost = gethostbyaddr((char *) &clientAddress.sin_addr.s_addr, 4, AF_INET);
-
-        printNumUsers();
+            printNumUsers();
+        }
     }
 
     WSACleanup();
@@ -108,82 +74,57 @@ int main() {
 
 // The function is created in a separate thread and serves
 // the connected client, regardless of the rest
-DWORD WINAPI serviceActiveClient(LPVOID clientSocket) {
-    SOCKET socket = ((SOCKET *) clientSocket)[0];
-
-    // Receive the number and return the client a representation in words
-    const unsigned long MAX_RECEIVE_BUFFER_LENGTH = 8;
-    char receiveBuffer[MAX_RECEIVE_BUFFER_LENGTH];
-    char* receiveBufCurrentPos = receiveBuffer;
+DWORD WINAPI serviceActiveClient(unsigned activeClientIdx) {
 
     #define IF_CONNECTION_CLOSED if (ret == 0) {\
-                                    numActiveClients--;\
+                                    activeClientIdx--;\
                                     std::cout << " disconnected" << std::endl;\
                                     printNumUsers();\
                                     return -1;\
                                  }
 
-    while ((unsigned long)(receiveBufCurrentPos - receiveBuffer) < MAX_RECEIVE_BUFFER_LENGTH)
+    while (true)
     {
-        int ret = recv(socket, (char *) receiveBufCurrentPos, 
-                               int(MAX_RECEIVE_BUFFER_LENGTH - (receiveBufCurrentPos - receiveBuffer)), 0);
+        // Receive the number and return the client a representation in words
+        const unsigned long MAX_RECEIVE_BUFFER_LENGTH = 8;
+        char receiveBuffer[MAX_RECEIVE_BUFFER_LENGTH];
+
+        int ret = recv(connections[activeClientIdx], receiveBuffer, sizeof(receiveBuffer), 0);
+
         if (ret == -1) {
             std::cout << "Server receive failed" << std::endl;
-            receiveBufCurrentPos = (char *) &ret;
         }
 
         IF_CONNECTION_CLOSED
 
-        receiveBufCurrentPos += ret;
+        const ushort MAX_SEND_BUFFER_LENGTH = 60;
+        char sendBuffer[MAX_SEND_BUFFER_LENGTH];
 
-        if ((unsigned long)(receiveBufCurrentPos - receiveBuffer) < MAX_RECEIVE_BUFFER_LENGTH)
-        {
-            std::cout << "[recv notice] attempt to receive " << MAX_RECEIVE_BUFFER_LENGTH << " bytes but ";
-            std::cout << (unsigned long)(receiveBufCurrentPos - receiveBuffer) << " received per call (";
-            std::cout << (MAX_RECEIVE_BUFFER_LENGTH - ((unsigned long)(receiveBufCurrentPos - receiveBuffer)));
-            std::cout << " left to receive)" << std::endl;
+        unsigned number = atoi(receiveBuffer);
+
+        if (number == 0) {
+            strcpy(sendBuffer, "ноль");
         }
-    }
 
-    const unsigned long MAX_SEND_BUFFER_LENGTH = 1024;
-    char sendBuffer[MAX_SEND_BUFFER_LENGTH] = {0};
+        if (number < 0) {
+            strcat(sendBuffer, "минус ");
+            number = -number;
+        }
 
-    unsigned long number = atoi(receiveBuffer);
+        const char* strNum = wnServer.format(number).c_str();
 
-    number = ntohl(number);
+        strcat(sendBuffer, strNum);
 
-    if (number == 0) {
-        strcpy(sendBuffer, "ноль");
-    }
+        ret = send(connections[activeClientIdx], sendBuffer, sizeof(sendBuffer), 0);
 
-    if (number < 0) {
-        strcat(sendBuffer, "минус ");
-        number = -number;
-    }
-
-    const char* strNum = wnServer.format((int)(number));
-
-    strcat(sendBuffer, strNum);
-
-    const char* sendBufCurrentPos = sendBuffer;
-
-    while ((unsigned long)(sendBufCurrentPos - sendBuffer) < MAX_SEND_BUFFER_LENGTH)
-    {
-        int ret = send(socket, (const char *) sendBufCurrentPos, 
-                               int(MAX_SEND_BUFFER_LENGTH - (sendBufCurrentPos - sendBuffer)), 0);
         if (ret == -1) {
             std::cout << "Server send failed" << std::endl;
-            sendBufCurrentPos = (char *) &ret;
         }
 
         IF_CONNECTION_CLOSED
-
-        sendBufCurrentPos += ret;
     }
 
-    PRESS_ANY_KEY
-
-    closesocket(socket);
+    closesocket(connections[activeClientIdx]);
     
     return 0;
 }
